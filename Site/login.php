daa29231e4932f01e081b3114be9fd88e30d7d12
Thiago Ferreira <thiago.ferreira@fcamara.com.br>
2016-11-22 22:59:09 +0000

    atualização

--- Site/login.php
+++ Site/login.php
@@ -2,19 +2,21 @@
 	
 	if (isset($_POST["usuario"])) {
 			$username = $_POST['usuario'];
-			$password = $_POST['senha'];
+			$password = md5($_POST['senha']);
 			
+			$senha_admin = md5('1234');
    
 			session_start();
 			$_SESSION['usuario'] =  strip_tags($username);
 			$_SESSION['senha'] = strip_tags($password);
 			if (isset($_SESSION['usuario'])&& isset($_SESSION['senha'])) {
             
-            if($_SESSION['usuario'] == 'adm' && $_SESSION['senha']=='1234'){
+            if($_SESSION['usuario'] == 'admin' && $_SESSION['senha']== $senha_admin){
 			header('Location: gestor.php');
             }
             else{
                 echo 'Usuário ou senha inválidos!';
+                header('Location: trabalheConosco.php');
 			}
     }
     }
